29bc260b7b3955537b7e4277191896d5226d6f6b
Sam Williams <shwilliams@vt.edu>
2022-08-11 17:13:03 -0400

    More test

--- main.js
+++ main.js
@@ -1661,7 +1661,7 @@ async function onStartXR(e) {
 
     let session = renderer.xr.getSession();
 
-    log (!session.device);
+    log (!session[Object.getOwnPropertySymbols(session)[1]]);
     let gamepads;
     if (!session.device)
         gamepads = session[Object.getOwnPropertySymbols(session)[1]].device.gamepads;
