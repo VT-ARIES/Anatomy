3b35760a96d4ae8cb67954ed70ccb8ed9e009873
Sam Williams <shwilliams@vt.edu>
2022-08-11 15:43:02 -0400

    Another test

--- main.js
+++ main.js
@@ -667,7 +667,7 @@ async function init() {
             
             
             //console.log(e.data)
-            log(e.data)
+            log(e)
 
             let weird_gamepad = e.data.gamepad;
             if (!weird_gamepad.hand)
